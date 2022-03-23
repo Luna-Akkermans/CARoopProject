75efe7600bde5426102dbdc1fc3648ba6c2a6e13
LMA <99044668@mydavinci.nl>
2022-03-23 15:58:55 +0100

    added-files+added pokemon base

--- classes/pokemon.js
+++ classes/pokemon.js
@@ -1,29 +1,37 @@
 class Pokemon {
-    constructor(name,energy,health) {
+    constructor(name, maxHealth, active, activeHealth, type, weakness, resistance, ...attacks){
         this.name = name;
-        this.energy = energy;
-        this.health = health;
+        this.maxHealth = maxHealth;
+        this.active;
+        this.activeHealth = activeHealth;
+        this.type = type;
+        this.weakness = weakness;
+        this.resistance = resistance;
+        this.attacks = attacks;
+    }
 
+    Hit(hitpoint){
+        if(hitpoint <= 0 ){
+            this.Remove();
         }
+    }
 
-
-
-    attack(energy) {
-        const damage = Math.floor(Math.random() * 21) - 5;
-        switch(energy.toLowerCase()) {
-            case "water":
-                console.log("water")
-                break;
-            case "grass":
-                console.log("grass")
-                break;
-            case "fire":
-                console.log("fire")
-                break;
-            case "electric":
-                    console.log("electric")
-                    break;
-
+    Attacking(Attacker, Defender) {
+        let damage;
+        if(Attacker.type === Defender.weakness.name){
+            damage = Attacker.damage * Math.floor(Math.random() * 5) + 1;
+            Defender.Hit(damage);
         }
+        else if(Attacker.type === Defender.resistance.name){
+            damage = Attacker.damage - Math.floor(Math.random() * 5) + 1; 
+            Defender.Hit(damage);
+        }
+        else{
+            Defender.Hit(damage);
+        }
+    }
+
+    Delete() {
+        this.active = false;
     }
 }
